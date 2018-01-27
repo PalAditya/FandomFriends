ac9bd4a8eb17e6d7f3b6fb3b41cc37374899f231
qawbecrdteyf <qawbecrdteyf@gmail.com>
2018-01-27 23:47:36 +0530

    ~Arshita~ ~Manisha~ Windows <3 Adyasha <3

--- app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
+++ app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
@@ -2,6 +2,11 @@ package com.app.lenovo.fandomfriends;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //int flag = 1;
+        EditText bio = (EditText) findViewById(R.id.Bio);
+        //ImageView img=(ImageView)findViewById(R.id.FandomFollowed4);
+        final ImageButton edit=(ImageButton)findViewById(R.id.imageButton);
+        edit.setOnClickListener(new View.OnClickListener() {
+
+            EditText bio = (EditText) findViewById(R.id.Bio);
+
+            //int flag = 1;
+            @Override
+            public void onClick(View view){
+                if(!bio.isEnabled()) {
+                    bio.setEnabled(true);
+                    bio.setCursorVisible(true);
+                    bio.setFocusableInTouchMode(true);
+                    edit.setImageResource(R.drawable.nrg);
+                }
+                else {
+                    bio.setEnabled(false);
+                    bio.setCursorVisible(false);
+                    bio.setFocusableInTouchMode(false);
+                    edit.setImageResource(R.drawable.sm);
+                }
+            }
+        });
     }
+
+
 }
