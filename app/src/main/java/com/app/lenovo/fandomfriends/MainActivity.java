75a751dcaa9f342ca9d84b1ec1bfa27f878f68dd
qawbecrdteyf <qawbecrdteyf@gmail.com>
2018-01-28 15:48:38 +0530

    The 2 fixes sorted

--- app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
+++ app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
@@ -1,5 +1,7 @@
 package com.app.lenovo.fandomfriends;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +12,18 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+    String biotext = "bio";
+    String bioedittext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //int flag = 1;
+        sharedPreferences = getSharedPreferences(biotext, Context.MODE_PRIVATE);
         EditText bio = (EditText) findViewById(R.id.Bio);
-        //ImageView img=(ImageView)findViewById(R.id.FandomFollowed4);
+        bioedittext = sharedPreferences.getString("bio", "Enter your bio here");
+        bio.setText(bioedittext);
         final ImageButton edit=(ImageButton)findViewById(R.id.imageButton);
         edit.setOnClickListener(new View.OnClickListener() {
 
@@ -31,11 +38,16 @@ public class MainActivity extends AppCompatActivity {
                     bio.setFocusableInTouchMode(true);
                     edit.setImageResource(R.drawable.nrg);
                 }
-                else {
+                else{
+                    String bioedit = bio.getText().toString();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(biotext, bioedit);
+                    editor.commit();
                     bio.setEnabled(false);
                     bio.setCursorVisible(false);
                     bio.setFocusableInTouchMode(false);
                     edit.setImageResource(R.drawable.sm);
+
                 }
             }
         });
